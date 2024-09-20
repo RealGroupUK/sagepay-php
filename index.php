01b4673a9482780f361d50dad1322b67040532a4
John Reid <john@realgroup.co.uk>
2024-09-20 12:36:12 +0100

    CHORE: Update Bootstrap to v5.3

--- index.php
+++ index.php
@@ -238,7 +238,7 @@ $merchantkey = $sagepay->getMerchantKey();
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
